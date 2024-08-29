8fd8d0a293ac611deb8397e7107083d5d2835934
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2024-08-29 11:24:35 +0700

    Colorized Outputs and Formatting

--- src/main.zig
+++ src/main.zig
@@ -6,18 +6,21 @@ const process = std.process;
 const mem = std.mem;
 const testing = std.testing;
 
+pub const std_options = .{
+    // Set the log level to info
+    .log_level = .info,
+};
 
 pub fn main() !void {
     var general_purpose_gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const gpa = general_purpose_gpa.allocator();
 
     defer {
-            const deinit_status = general_purpose_gpa.deinit();
-            //fail test; can't try in defer as defer is executed after we return
-            if (deinit_status == .leak) {
-                std.debug.print("Memory leak detected\n", .{});
-            }
+        const deinit_status = general_purpose_gpa.deinit();
+        if (deinit_status == .leak) {
+            std.debug.print("Memory leak detected\n", .{});
         }
+    }
 
     var args = try std.process.argsWithAllocator(gpa);
     defer args.deinit();
@@ -60,16 +63,13 @@ pub fn main() !void {
         if (entry.kind != .file or !mem.endsWith(u8, entry.name, ".in")) continue;
 
         const caseName = entry.name[0 .. entry.name.len - 3];
-        std.log.info("Running Case {s} ...", .{caseName});
+        std.log.info("Running Case \x1b[1;36m{s}\x1b[0m...", .{caseName});
 
         // Run main.py with input file
         const input_path = try std.fmt.allocPrint(gpa, "{s}/{s}", .{ input_pathdir, entry.name });
+        defer gpa.free(input_path);
         const output_path = try std.fmt.allocPrint(gpa, "{s}/{s}.out", .{ output_pathdir, caseName });
-
-        defer {
-            gpa.free(input_path);
-            gpa.free(output_path);
-        }
+        defer gpa.free(output_path);
 
         var child = process.Child.init(&.{ python_cmd, "main.py" }, gpa);
 
@@ -119,9 +119,9 @@ pub fn main() !void {
         const result = try compareFiles(gpa, expected_path, actual_path);
 
         if (result) {
-            std.log.info("Results: Matched, Good job!", .{});
+            std.log.info("Results: \x1b[1;32mMatched\x1b[0m, Good job!", .{});
         } else {
-            std.log.err("Results: NOT Matched, Please check your code!", .{});
+            std.log.err("Results: \x1b[1;31mNOT\x1b[0m Matched, Please check your code!", .{});
             try showDiff(gpa, expected_path, actual_path);
         }
 
@@ -152,15 +152,15 @@ pub fn showDiff(allocator: std.mem.Allocator, file1: []const u8, file2: []const 
     const content2 = try std.fs.cwd().readFileAlloc(allocator, file2, std.math.maxInt(usize));
     defer allocator.free(content2);
 
-    var lines1 = std.mem.tokenize(u8, content1, "\n");
-    var lines2 = std.mem.tokenize(u8, content2, "\n");
+    var lines1 = std.mem.tokenizeSequence(u8, content1, "\n");
+    var lines2 = std.mem.tokenizeSequence(u8, content2, "\n");
 
     const stdout = std.io.getStdOut().writer();
 
     var line_number: usize = 1;
 
-    try stdout.print("Line | {s:<40} | {s}\n", .{"Expected", "Got"});
-    try stdout.print("---- | {s:-<40} | {s:-<40}\n", .{"", ""});
+    try stdout.print("Line | {s:<40} | {s}\n", .{ "Expected", "Got" });
+    try stdout.print("---- | {s:-<40} | {s:-<40}\n", .{ "", "" });
 
     while (true) {
         const line1 = lines1.next();
@@ -171,9 +171,9 @@ pub fn showDiff(allocator: std.mem.Allocator, file1: []const u8, file2: []const 
         if (line1) |l1| {
             if (line2) |l2| {
                 if (std.mem.eql(u8, l1, l2)) {
-                    try stdout.print("{d:4} | {s}\n", .{ line_number, l1 });
-                } else {
                     try stdout.print("{d:4} | {s:<40} | {s}\n", .{ line_number, l1, l2 });
+                } else {
+                    try stdout.print("{d:4} | {s:<40} | \x1b[1;31m{s}\x1b[0m\n", .{ line_number, l1, l2 });
                 }
             } else {
                 try stdout.print("{d:4} | {s:<40} | \n", .{ line_number, l1 });
